caa00afa531c5ce141fcfc0a1a632d8f131041d8
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2023-12-18 11:56:46 -0500

    INT-19330: 4.1 MP3 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2023100400;
+$plugin->version   = 2023121800;
 $plugin->requires  = 2022112800;
 $plugin->release   = '4.1.3';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'block_snapfeeds';
 $plugin->dependencies = [
-    'theme_snap' => '2023100400'
+    'theme_snap' => '2023121800'
 ];
